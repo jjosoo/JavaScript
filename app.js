281c033c54d3f3f165976d9a32c03d2a214f645d
jjosoo <130520024+jjosoo@users.noreply.github.com>
2023-10-30 19:14:46 +0900

    1030

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
 // 링크
 const link = document.querySelector("a");
 
+if(localStorage.value === ""){
+    
+}
+
 function onLoginSubmit (event){
     event.preventDefault();
+
     const username = loginInput.value;
+    localStorage.setItem("username",username);
+
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     
+    // greeting.innerText = "Hello " + username;
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 // 링크
