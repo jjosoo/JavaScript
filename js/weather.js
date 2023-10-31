70c83b03c21631b37fd8c40469fafac9e8297b04
jjosoo <130520024+jjosoo@users.noreply.github.com>
2023-10-31 16:50:05 +0900

    1031

---
+++ js/weather.js
@@ -0,0 +1,13 @@
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    console.log("you live in", lat, lng); 
+
+    console.log(position);
+}
+
+function onGeoError(){
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
