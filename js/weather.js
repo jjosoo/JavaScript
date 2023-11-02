a8be8c912ac963dadd310a5e6e6cc695d2b96eb7
jjosoo <130520024+jjosoo@users.noreply.github.com>
2023-11-02 09:39:19 +0900

    1102

--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,18 @@
+const API_KEY = "cd17be4f7ebd19e19b66f608ce69c04b";
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("you live in", lat, lng); 
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
-    console.log(position);
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
 }
 
 function onGeoError(){
